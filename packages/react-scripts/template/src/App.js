bc9686f01cf61517c3054cc9dba77c54257a9195
Grace <g7@Graces-MBP.fios-router.home>
2019-11-13 14:29:04 -0500

    Update template.

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+    console.log(process.env);
+  }
+  render() {
+    return (
+      <div className="myAppName">
+        <div className="jumbotron">
+          <h1 class="display-4">Hello, world!</h1>
+          <p class="lead">
+            <a className="btn btn-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+          </p>
+          <hr className="my-4"></hr>
+          <p><code>Create a .env file at the root of your project with several REACT_APP_SOME_UNIQUE_IDENTIFIER's</code></p>
+          <p><mark>Note: CRA already includes support for custom env variables if you're open to prefixing their names with REACT_APP</mark></p>
+          <hr className="my-4"></hr>
+          <p>Update title text & app icon in <code>public/</code></p>
+          <p>Edit <code>src/App.js</code> and save to reload.</p>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
